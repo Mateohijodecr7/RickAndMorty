16ff52bafeb5c4a807de8f99b04496ffe446daad
Mateohijodecr7 <majajo12377@gmail.com>
2023-02-06 12:13:39 -0500

    first commit

---
+++ src/Images/Imagen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "./image.css"
+
+const Imagen = () => {
+    return <img
+    className="img"
+    src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty.png">
+    </img>
+}
+
+export default Imagen
